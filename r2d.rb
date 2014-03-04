cc5fdf4b40a3e0b41cf90f0d2df9e39ec3b923c2
Jason Walsh <jwalsh@booster.com>
2014-03-04 21:08:46 +0800

    Scaffolding for
      http://en.wikipedia.org/wiki/Roman_numerals

--- r2d.rb
+++ r2d.rb
@@ -1,24 +1,31 @@
 # http://en.wikipedia.org/wiki/Roman_numerals
 
 class RomanNumeralConverter
+
+
+  CONVERSION_TABLE = [
+    [1000, 'M'],
+    [500, 'D'],
+    [100, 'C'],
+    [50, 'L'],
+    [40, 'XL'],
+    [19, 'XIX'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I']
+  ]
+
   def convert(n)
 
     roman = ""
 
-    while n >= 10
-      roman << "X"
-      n -= 10
-    end
-
-    while n >= 5
-      roman << "V"
-      n -= 5
-    end
-
-    # Fallthrough
-    while n >= 1
-      roman << "I"
-      n -= 1
+    CONVERSION_TABLE.each do |number, glyph|
+      while n >= number
+        roman << glyph
+        n -= number
+      end
     end
 
     roman
